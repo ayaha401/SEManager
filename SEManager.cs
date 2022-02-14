c7669371dc53ec723b4c8d4211358ad7e40591e0
ayaha401 <oshizawadai.1192@gmail.com>
2022-02-14 20:54:46 +0900

    改良

--- SEManager.cs
+++ SEManager.cs
@@ -4,32 +4,95 @@ using UnityEngine;
 
 public class SEManager : MonoBehaviour
 {
-    [SerializeField, Tooltip("Effect")]
-    List<AudioClip> SEObjList = new List<AudioClip>();
+    [SerializeField, Tooltip("SE")]
+    List<AudioClip> _audioList = new List<AudioClip>();
 
-    private static Dictionary<string, AudioClip> SEObjDic = new Dictionary<string, AudioClip>();
-    
-    void Awake() 
+    private static Dictionary<string, AudioClip> _audioDictionary = new Dictionary<string, AudioClip>();
+    private static AudioSource _audioSource = null;
+
+    public static bool SEManagerMaked = false;
+    public static float SE_Volume = 0.0f;
+
+    void Awake()
     {
-        foreach (AudioClip item in SEObjList)
+        if (SEManagerMaked == false)
         {
-            if(SEObjDic.ContainsKey(item.name) == true)
+            DontDestroyOnLoad(this);
+            SEManagerMaked = true;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        foreach (AudioClip item in _audioList)
+        {
+            if (_audioDictionary.ContainsKey(item.name) == true)
             {
                 return;
             }
-            SEObjDic.Add(item.name, item);
+            _audioDictionary.Add(item.name, item);
         }
-    } 
 
-    public static AudioClip SendSE(string effectName)
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
     {
-        if(SEObjDic.ContainsKey(effectName) != false)
+        SE_Volume = _audioSource.volume;
+    }
+
+    // ディクショナリから指定されたAudioClipを返す
+    private static AudioClip GetAudioClip(string audioName)
+    {
+        if (_audioDictionary.ContainsKey(audioName) == true)
         {
-            return SEObjDic[effectName];
+            return _audioDictionary[audioName];
         }
         else
         {
             return null;
         }
+    }
+
+    // 音にランダムなピッチを加算してランダム性を追加する。
+    private static void AudioRandomize(float pitchRange)
+    {
+        _audioSource.pitch = 1.0f + Random.Range(-pitchRange, pitchRange);
+    }
+
+    // SEを鳴らす
+    // 重複しないで再生可能
+    public static void AudioPlay(string audioName, float volume = 1.0f, float pitchRange = 0.0f)
+    {
+        AudioClip clip;
+        clip = GetAudioClip(audioName);
+        if (clip != null)
+        {
+            AudioRandomize(pitchRange);
+            _audioSource.clip = clip;
+            _audioSource.volume = volume;
+            _audioSource.Play();
+        }
+    }
+
+    // 重複して再生可能
+    public static void AudioPlayOneShot(string audioName, float volume = 1.0f, float pitchRange = 0.0f)
+    {
+        AudioClip clip;
+        clip = GetAudioClip(audioName);
+        if (clip != null)
+        {
+            AudioRandomize(pitchRange);
+            _audioSource.volume = volume;
+            _audioSource.PlayOneShot(clip);
+        }
+    }
+
+    // volume : 0~10
+    public static void SetVolume(float volume)
+    {
+        SE_Volume = volume * 0.1f;
+        _audioSource.volume = SE_Volume;
     }
 }
